7a3c6df1074698840c5112c7d905f1d2ba36a3e6
Hangar555 <ivan.tigger1@gmail.com>
2017-06-16 14:05:08 +0200

    Update fr.java

--- src/eu/hangar/cmds/fr.java
+++ src/eu/hangar/cmds/fr.java
@@ -9,6 +9,7 @@ import org.bukkit.inventory.ItemStack;
 
 import eu.hangar.CommandInfo;
 import eu.hangar.GameCommand;
+import eu.hangar.VillagerMenu;
 
 @CommandInfo(description = "fr", usage = "<fr>", aliases = { "frandom", "fr" }, op=true)
 public class fr extends GameCommand{
@@ -19,24 +20,26 @@ public class fr extends GameCommand{
 	
 	
 	
-	Random random = new Random();
-	int Chance = random.nextInt(200);
-	if(p.hasPermission("ffa.admin")){ 
-	if(!p.hasPermission("ffa.forceem")){
-	
-	if(Chance == 400 || Chance == 200 || Chance == 100 || Chance == 50 || Chance == 25 || Chance == 500) {
-	
+    	Random random = new Random();
+    	int Chance = random.nextInt(100);
+    	 
+    
+    	     if(p.hasPermission("ffa.admin"))  {
+    	    	 
+    	     
+    	if(Chance > 55) {
+    	
+    	 
+    	p.getInventory().addItem(new ItemStack(Material.EMERALD));
+    	}else{
+    		p.sendMessage("Nothing!");
+    	}
 	 
-	p.getInventory().addItem(new ItemStack(Material.EMERALD));
-	
-	} else {
-	   p.sendMessage(ChatColor.RED+"Bad luck! No emerald for you!");
-	}
-	 }else{
-		 
-		 p.sendMessage(ChatColor.GREEN + "Good News! You got an emerald!");
-		 p.getInventory().addItem(new ItemStack(Material.EMERALD)); 
-	 }
+}else{
+	p.sendMessage(ChatColor.DARK_RED + "[ERROR]" + ChatColor.RED + " Not enough permissions!");
+}
+}
+}
 	 
 }else{
 	p.sendMessage(ChatColor.DARK_RED + "[ERROR]" + ChatColor.RED + " Not enough permissions!");
